94c7ef60d3261023a548bcb590e13e627b9f0143
Shravani311003 <126446851+Shravani311003@users.noreply.github.com>
2024-05-05 22:57:00 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,32 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    left_pointer, right_pointer = 0, 0
+
+    while left_pointer < len(left) and right_pointer < len(right):
+        if left[left_pointer] < right[right_pointer]:
+            result.append(left[left_pointer])
+            left_pointer += 1
+        else:
+            result.append(right[right_pointer])
+            right_pointer += 1
+
+    result.extend(left[left_pointer:])
+    result.extend(right[right_pointer:])
+    return result
+
 def bubble_sort(arr):
     n=len(arr)
     for i in range(n):
@@ -55,3 +84,8 @@ arr = input_list[:]
 
 arr = quick_sort(arr)
 print("Quick Sorted List:", arr)
+
+arr = input_list[:]
+
+arr = merge_sort(arr)
+print("Merge Sorted List:", arr)
